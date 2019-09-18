98010dacf964868c59d7029a54f47121d7ae2cc7
joelfooxj <joel.foo@mail.utoronto.ca>
2019-09-17 20:21:21 -0400

    Updated phone_loop.c

--- lab2/phone_loop.c
+++ lab2/phone_loop.c
@@ -4,18 +4,17 @@ int main(){
     int return_flag = 0;
     char phone[11];
     int in_int; 
-    scanf("%s" ,phone); 
+    scanf("%s", phone); 
     // see that a pointer/addres must be passed into scanf
     while(1){
-        scanf("%d", &in_int); 
+        if (scanf("%d", &in_int)==EOF){return return_flag;} 
         if (in_int == -1){
-            printf("%s", phone);
+            printf("%s\n", phone);
         } else if (in_int >= 0 && in_int <= 9){
-            printf("%c", phone[in_int]); 
+            printf("%c\n", phone[in_int]); 
         } else {
-            printf("ERROR"); 
+            printf("ERROR\n"); 
             return_flag = 1;
         }
     }
-    return return_flag;        
 }
