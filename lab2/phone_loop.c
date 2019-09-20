fad1c165de411527805793c2c1100ade16c2d647
Foo Joel <foojoel@teach.cs.toronto.edu>
2019-09-20 15:33:46 -0400

    updated phone_loop.c

--- lab2/phone_loop.c
+++ lab2/phone_loop.c
@@ -5,11 +5,7 @@ int main(){
     char phone[11];
     int in_int; 
     scanf("%10s", phone); 
-    int c; 
-    while((c = getchar()) != '\n' && c != EOF) 
-    // see that a pointer/addres must be passed into scanf
     while(1){
-        while((c = getchar()) != '\n' && c != EOF) 
         if (scanf("%d", &in_int)==EOF){return return_flag;} 
         if (in_int == -1){
             printf("%s\n", phone);
