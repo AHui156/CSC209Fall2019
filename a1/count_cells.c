cfc1cf19fd1bc040d0a6aee4ca460407a2399d10
joelfooxj <joel.foo@mail.utoronto.ca>
2019-09-26 23:35:50 -0400

    Added a1 c files

--- a1/count_cells.c
+++ a1/count_cells.c
@@ -1,18 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
+#include <errno.h> 
 void read_image(int num_rows, int num_cols, 
                 int arr[num_rows][num_cols], FILE *fp);
                 
 void print_image(int num_rows, int num_cols, int arr[num_rows][num_cols]);
 
 // Remember to include the function prototype for count_cells
-
+int count_cells(int num_rows, int num_cols, int arr[num_rows][num_cols]);
 
 int main(int argc, char **argv) {
     // Print a message to stderr and exit with an argument of 1 if there are
     // not the right number of parameters, or the second argument is not -p
-
-
+    if(argc > 3 || argc == 1){
+        fprintf(stderr, "Incorrect number of arguments\n"); 
+        return 1;
+    }
+    else if(argc == 3 && strcmp(argv[2], "-p") != 0){
+        fprintf(stderr, "-p is the optional argument\n" ); 
+        return 1;
+    }
+    
+    int rows = 0, columns = 0; 
+    FILE* fp = fopen(argv[1], "r"); 
+    if (fp != NULL){
+        fscanf(fp, "%d %d\n", &rows, &columns); 
+        int a[rows][columns]; 
+        read_image(rows, columns, a, fp);  
+        if (argc == 3){ print_image(rows, columns, a);}
+        printf("Number of Cells is %d\n", count_cells(rows, columns, a));
+        fclose(fp);
+    } else {
+        fprintf(stderr, "Error opening file: %s\n", strerror(errno));
+    }    
     return 0;
 }
--- a1/image.c
+++ a1/image.c
@@ -1,16 +1,68 @@
 #include <stdio.h>
-    
 /* Reads the image from the open file fp into the two-dimensional array arr 
  * num_rows and num_cols specify the dimensions of arr
  */
 void read_image(int num_rows, int num_cols, 
                 int arr[num_rows][num_cols], FILE *fp) {
-
+    for(int i = 0; i < num_rows; i++){
+        for(int m = 0; m < num_cols; m++){
+            fscanf(fp, "%d ", &arr[i][m]);
+        }
+    }
 }
 
 /* Print to standard output the contents of the array arr */
 void print_image(int num_rows, int num_cols, int arr[num_rows][num_cols]) {
+    for(int i = 0; i < num_rows; i++){
+        for(int m = 0; m < num_cols; m++){
+            printf("%d ", arr[i][m]);
+        }
+        printf("\n"); 
 
+    }
 }
 
-/* TODO: Write the count_cells function */
+/*
+* Gets the current cell coordinates and recursively checks adjacent cells until 
+* all cells that are connected are set to 0.
+*/
+void set_cells(int num_rows, int num_cols, int i, int m, int arr[num_rows][num_cols]) {
+    if (i - 1 >= 0){
+        if(arr[i - 1][m] == 255) {
+            arr[i - 1][m] = 0;
+            set_cells(num_rows, num_cols, i - 1, m, arr);
+        }
+    }
+    if (i + 1 < num_rows){
+        if(arr[i + 1][m] == 255) {
+            arr[i + 1][m] = 0;
+            set_cells(num_rows, num_cols, i + 1, m, arr);
+        }
+    } 
+    if (m - 1 >= 0){
+        if (arr[i][m - 1] == 255) {
+            arr[i][m - 1] = 0;
+            set_cells(num_rows, num_cols, i, m - 1, arr);
+        }
+    }
+    if (m + 1 < num_cols){
+        if (arr[i][m + 1] == 255) {
+            arr[i][m + 1] = 0;
+            set_cells(num_rows, num_cols, i, m + 1, arr);
+        }
+    }
+}
+
+int count_cells(int num_rows, int num_cols, int arr[num_rows][num_cols]) {
+    int cells = 0;
+    for(int i = 0; i < num_rows; i++){
+        for(int m = 0; m < num_cols; m++){
+            if (arr[i][m] == 255){ 
+                cells++;
+                arr[i][m] = 0; 
+                set_cells(num_rows, num_cols, i, m, arr); 
+            }
+        }
+    }
+    return cells;
+}
